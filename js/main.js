5c9c9e9dc237b7bf55de720eb46fe9e293250a1a
Tiago Duarte <tiagoduarte.m@hotmail.com>
2024-05-13 15:45:55 -0300

    geral

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.slider_principal').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplayspeed:5000.
+      });
+
+
+
